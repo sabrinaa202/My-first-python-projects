d707d0aeb1f5445def2db3e8ef3ca64c358e6158
sabrinaa202 <sabrinaaah202@gmail.com>
2026-07-09 04:23:04 +0600

    simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,11 @@
+first_number=int(input("enter the first number: "))
+second_number=int(input("enter the second number: "))
+addition= first_number+second_number
+subtraction= first_number-second_number
+multiplication=first_number*second_number
+division=first_number/second_number
+print(
+"Addition=", addition, 
+"\nSubtraction=", subtraction, 
+"\nMultiplication=", multiplication,
+"\nDivision=",  division)
