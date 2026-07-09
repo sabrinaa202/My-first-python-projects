d6eda5063a330e7bff9ae2facfc2c62fb3db6609
Sabrina <sabrinaaah202@gmail.com>
2026-07-09 17:53:45 +0600

    Delete Python Projects directory

--- Python Projects/number_guessing_game.py
+++
@@ -1,7 +0,0 @@
-import random
-guessing_number =int(input("guess a random number between 1 to 500:  "))
-secret_number=random.randint(1, 500)
-if guessing_number==secret_number:
-    print("Congratulations! You guessed the correct number!")
-else :
-    print("Incorrect.the correct number is ",secret_number)
